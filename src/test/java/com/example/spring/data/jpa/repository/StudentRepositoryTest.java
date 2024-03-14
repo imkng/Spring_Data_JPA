5c1efad60ffe6695afb3238bcdca6d4a7ba970bc
krnand <1india.kng@gmail.com>
2024-03-14 12:33:24 +0530

    added studentrepository

---
+++ src/test/java/com/example/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.spring.data.jpa.repository;
+
+import com.example.spring.data.jpa.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+//@DataJdbcTest:- it will insert the data to db and flush it out after it gets over
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void insertStudentTest(){
+        Student student = Student.builder()
+                .emailId("krishna3@gmail.com")
+                .firstName("krishna")
+                .lastName("nand")
+                .guardianEmail("@abc@gmaik.com")
+                .guardianName("@abc")
+                .guardianPhone("9809eu9uw9e")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+        System.out.println(studentList);
+    }
+
+}
