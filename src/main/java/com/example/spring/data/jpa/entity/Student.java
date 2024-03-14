47e280e6ab0e6d2cc779312cd6455cd010d991d7
krnand <1india.kng@gmail.com>
2024-03-14 12:09:17 +0530

    added sequence generator for id

--- src/main/java/com/example/spring/data/jpa/entity/Student.java
+++ src/main/java/com/example/spring/data/jpa/entity/Student.java
@@ -1,9 +1,30 @@
 package com.example.spring.data.jpa.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tbl_student")
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
